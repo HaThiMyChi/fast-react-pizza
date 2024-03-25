e4ff7a85bbae71c8ee9e7bea75b75f2d8d3e8a92
MyChi <mychikhtn@gmail.com>
2024-03-25 15:54:05 +0700

    Setting up a professional file structure and a new way of implementing Routes

---
+++ src/features/order/CreateOrder.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+CreateOrder.propTypes = {
+    
+};
+
+function CreateOrder(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CreateOrder;
---
+++ src/ui/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Home.propTypes = {
+    
+};
+
+function Home(props) {
+    return (
+        <div>
+            <h1>The best pizza.
+                <br/>
+                Straight out of the oven, straight to you
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
