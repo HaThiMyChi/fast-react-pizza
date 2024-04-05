92c2e83fb168ceccbcfd9ae8e0c1f0f3b275ea4b
MyChi <mychikhtn@gmail.com>
2024-04-05 16:21:09 +0700

    Building the Cart Overview with Redux Selectors

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getTotalCartPrice, getTotalCartQuantity } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 CartOverview.propTypes = {
     
 };
 
 function CartOverview(props) {
-    
+    const totalCartQuantity = useSelector(getTotalCartQuantity);
+    const totalCartPrice = useSelector(getTotalCartPrice);
+
+    if (!totalCartQuantity) return null;
+
     return (
         <div className='bg-stone-800 text-sm px-4 py-4 uppercase sm:px-6 md:text-base flex items-center justify-between text-stone-200'>
             <p className='text-stone-300 font-semibold space-x-4 sm:space-x-6'>
-                <span>23 pizzas</span>
-                <span>$23.45</span>
+                <span>{totalCartQuantity} pizzas</span>
+                <span>{formatCurrency(totalCartPrice)}</span>
             </p>
             <Link to="/cart">Open cart &rarr;</Link>
         </div>
